b7e38c92072c887a1a940e30ec613a8ab9ca97e5
Nina Margrethe Smørsgård <nina.m.smorsgard@gmail.com>
2015-02-03 17:00:07 +0100

    Updated iface

--- oving-1/TicTacToeRemote.java
+++ oving-1/TicTacToeRemote.java
@@ -2,12 +2,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface TicTacToeRemote extends Remote {
-    public void newGame() throws RemoteException;
+    public void newGame(TicTacToe ticTacToe) throws RemoteException;
     public void quitGame() throws RemoteException;
 
     public void setMark(int x, int y) throws RemoteException;
 
     public void setOpponent(TicTacToeRemote remote) throws RemoteException;
-    public void setOpponentMark() throws RemoteException;
+    public void setOpponentMark(char opponentMark) throws RemoteException;
 
 }
